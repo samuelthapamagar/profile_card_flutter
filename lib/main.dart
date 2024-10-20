2cde876dd0569c67e7b37612fac9e12f29e2350e
samuelthapamagar <samuelthapamagar77@gmail.com>
2024-10-20 10:35:09 +0545

    add figma ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// Figma UI : https://www.figma.com/design/YE208Yw8cVQRE8gA5viceg/Profile-Card-UI?node-id=0-1&node-type=canvas&t=wwUlfJnhCckVwaqB-0
+
 void main() {
   runApp(MaterialApp(
     home: Scaffold(),
